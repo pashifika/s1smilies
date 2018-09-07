0ce8df67a2397926b523bdd69300fb0e432798c9
pashifika <pashifika.mkii@gmail.com>
2018-09-07 16:16:35 +0800

    fix: Download thread saved local file name exists already error. new: Interactive cli renewal.

--- main.go
+++ main.go
@@ -7,13 +7,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"go.uber.org/atomic"
 	"os"
 	"path/filepath"
 	"regexp"
-	"sync"
-
-	"go.uber.org/atomic"
 	"s1smilies/libs"
+	"sync"
 )
 
 const (
@@ -39,16 +38,12 @@ func main() {
 
 	fmt.Print("数据加载完毕！\n\n")
 	fmt.Println("请选择需要下载的麻将脸类型")
-	var dlList []string
+	var dlList, allList []string
 	for stID, v := range sdl.Stype {
 		fmt.Printf("ID:%s\t\tName:%s\t\tTotal:%d\n", stID, v.Name, len(sdl.DLlist[stID]))
-		dlList = append(dlList, stID)
+		allList = append(allList, stID)
 	}
-	fmt.Print("提示: 输入 'all' 下载全部类型\n\n")
-	run_mode := question("请输入需要下载的麻将脸类型ID: ", sdl)
-	if run_mode != "all" {
-		dlList = []string{run_mode}
-	}
+	fmt.Print("提示: 输入 'all' 下载全部类型，输入 'list' 列出所有麻将脸类型，输入 'exit' 退出程序\n\n")
 
 	// Check download dir
 	os_info := os.Args[0]
@@ -61,67 +56,77 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("下载程序干活中，请勿关闭本窗口...")
-	// multi limit setting start
-	var wg sync.WaitGroup
-	semaphore := make(chan int, dlThreads)
-	// multi limit setting end
-	var dlURL, filePath string
-	for _, stID := range dlList {
-		var count_err, count atomic.Int64
-		for _, fdl := range sdl.DLlist[stID] {
-			dlURL = smiliesURL_root + sdl.Stype[stID].DirPath + "/" + fdl.FileName
-
-			// Make download thread
-			semaphore <- 1
-			wg.Add(1)
-			go func(url string) {
-				defer func() {
-					<-semaphore
-					wg.Done()
-				}()
-				filePath = filepath.Join(dlDir, fdl.Name)
-				err = libs.DownloadFile(filePath, url)
-				if err != nil {
-					fmt.Printf("file %s download error, %s", url, err.Error())
-					count_err.Add(1)
-				} else {
-					count.Add(1)
-				}
-			}(dlURL)
-		}
-
-		// Wait all download thread complete
-		wg.Wait()
-		fmt.Printf("类型：%s\t下载成功：%d\t下载失败：%d", sdl.Stype[stID].Name, count.Load(), count_err.Load())
-	}
-}
-
-// Interactive cli for download smiliesType
-func question(q string, sdl *libs.SmiliesDL) string {
-	fmt.Print(q)
-
-	var res string
+	// Interactive cli for download smiliesType
+	fmt.Print("请输入需要下载的麻将脸类型ID: ")
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		input := scanner.Text()
-		if input == "all" {
-			res = input
-			break
-		}
-		if _, ok := sdl.Stype[input]; !ok {
-			fmt.Println("输入错误, 请再次输入！")
-			fmt.Print(q)
-		} else {
-			res = input
-			break
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
+		switch input {
+		case "exit": // Exit program
+			return
+		case "list": // List all smilies type
+			fmt.Println("请选择需要下载的麻将脸类型")
+			for stID, v := range sdl.Stype {
+				fmt.Printf("ID:%s\t\tName:%s\t\tTotal:%d\n", stID, v.Name, len(sdl.DLlist[stID]))
+			}
+			fmt.Print("提示: 输入 'all' 下载全部类型，输入 'list' 列出所有麻将脸类型，输入 'exit' 退出程序\n\n")
+			fmt.Print("请输入需要下载的麻将脸类型ID: ")
+		default: // Download process
+			if input != "all" {
+				dlList = []string{input}
+			} else {
+				dlList = allList
+			}
+			if _, ok := sdl.Stype[input]; !ok {
+				fmt.Println("输入错误, 请再次输入！")
+				fmt.Print("请输入需要下载的麻将脸类型ID: ")
+				continue
+			}
 
-	return res
+			fmt.Println("下载程序干活中，请勿关闭本窗口...")
+			// multi limit setting start
+			var wg sync.WaitGroup
+			semaphore := make(chan int, dlThreads)
+			// multi limit setting end
+			for _, stID := range dlList {
+				var count_err, count atomic.Int64
+				for _, fdl := range sdl.DLlist[stID] {
+					// Make download thread
+					wg.Add(1)
+					semaphore <- 1
+					go func(url, saveName string) {
+						defer func() {
+							<-semaphore
+							wg.Done()
+						}()
+						filePath := filepath.Join(dlDir, saveName)
+
+						// Check local smilies is exists
+						if exists(filePath) {
+							count.Add(1)
+							return
+						}
+
+						// Download remote smilies to local
+						err = libs.DownloadFile(filePath, url)
+						if err != nil {
+							fmt.Printf("file %s download error, %s", url, err.Error())
+							count_err.Add(1)
+						} else {
+							count.Add(1)
+						}
+					}(smiliesURL_root + sdl.Stype[stID].DirPath + "/" + fdl.FileName, fdl.Name)
+				}
+
+				// Wait all download thread complete
+				wg.Wait()
+				fmt.Printf("类型：%s\t下载成功：%d\t下载失败：%d\n\n", sdl.Stype[stID].Name, count.Load(), count_err.Load())
+			}
+
+			fmt.Print("提示: 输入 'all' 下载全部类型，输入 'list' 列出所有麻将脸类型，输入 'exit' 退出程序\n")
+			fmt.Print("请输入需要下载的麻将脸类型ID: ")
+		}
+	}
 }
 
 // Exists returns whether the given file or directory exists or not
